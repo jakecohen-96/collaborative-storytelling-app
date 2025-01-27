a8feac72b6f7d78fe5474317a9942b777bb241a5
jakecohen-96 <yakop.96@gmail.com>
2025-01-27 20:10:42 +0200

    ammended frontend axiosInstance to live render website

--- frontend/src/utils/axiosInstance.ts
+++ frontend/src/utils/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001",
+    baseURL: "https://collaborative-storytelling-app.onrender.com",
     withCredentials: true, 
 });
 
