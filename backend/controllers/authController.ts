25b411023b1c43c11e3b5890ed9b98d20d673e49
jakecohen-96 <yakop.96@gmail.com>
2025-01-27 20:05:34 +0200

    render test 2

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import pool from "../db/connection";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 const SALT_ROUNDS = 10;
